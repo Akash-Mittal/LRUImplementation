c96aaeb6050da28417910eb24e523023fea308f1
Akash Mittal <Akash_Mittal@external.mckinsey.com>
2018-05-23 12:16:01 +0530

    Intitial Commit.

---
+++ src/main/java/com/lru/LruImplementationApplication.java
@@ -0,0 +1,12 @@
+package com.lru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LruImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LruImplementationApplication.class, args);
+	}
+}
